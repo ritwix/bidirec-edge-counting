17d891343f3a99d0bca2ccd149228919616c4adc
clarkmollencop <cmollencop@gmail.com>
2022-06-10 11:42:15 -0400

    implement data structure for counting

--- bidirec_script.py
+++ bidirec_script.py
@@ -36,27 +36,34 @@ def dup_edges(graph_df, delta, period=12):
 # and t2 to go all the way through the duplicates
 # also want to structure to go the duplicates for t2
 # length of thing = unique_len*delta + (unique_len*(unique_len+1))/2 (from math on my desk whiteboard if asked)
-def setup_data_struc(graph, graph_dup, delta):
+def setup_data_struc(graph, graph_dup, delta, period):
     struc = {'t1': [], 't2': [], 'count': []}
     # number of unique timestamps in the original graph
-    unique = pd.unique(graph['timestamp'])
+    #unique = pd.unique(graph['timestamp'])
     # number of unique timestamps in the graph with extra timestamps
-    unique_dup = pd.unique(graph_dup['timestamp'])
-    unique_dup_len = len(unique_dup)
-    unique = sorted(unique)
-    unique_dup = sorted(unique_dup)
-    unique_len = len(unique)
+    #unique_dup = pd.unique(graph_dup['timestamp'])
+    #unique_dup_len = len(unique_dup)
+    #unique = sorted(unique)
+    #unique_dup = sorted(unique_dup)
+    #unique_len = len(unique)
     # length of thing = unique_len*delta + (unique_len*(unique_len+1))/2
-    num_pairs = int(unique_len*delta + (unique_len*(unique_len+1))/2)
+    #num_pairs = int(unique_len*delta + (unique_len*(unique_len+1))/2)
 
+    # new way includes all possible timestamps for easier math
+    num_pairs = period*delta + (period * (period+1))//2
+
+    l = []
+    for i in range(1, period + delta + 1):
+        l.append(i)
+    
     for i in range(num_pairs):
         struc['count'].append(0)
     
-    for i in range(1, (unique_len+1)):
+    for i in range(1, (period + 1)):
         j = i
-        while j < (unique_dup_len+1):
+        while j < (period + delta + 1):
             struc['t1'].append(i)
-            struc['t2'].append(unique_dup[j-1])
+            struc['t2'].append(l[j-1])
             j += 1
     
     # convert this dict to a pandas dataframe for easy plotting
@@ -76,6 +83,8 @@ def bidirecCountAndStore(graph, delta, period, struc_df):
     delta: time-interval within which bidirectional edges must be counted.
     for each edge with timestamp t, computes the number of reverse edges within t + delta and stores this in
     its edge attribute.
+    period: period within which the graph repeats 
+    struc_df: the dataframe within which we hold the timestamps for plotting
     Returns the graph with the additional edge attribute
     """
     
@@ -97,10 +106,14 @@ def bidirecCountAndStore(graph, delta, period, struc_df):
                 t1 = edges[i][2]['timestamp']
                 t2 = edges[j][2]['timestamp']
                 print(f't1: {t1}, t2: {t2}')
+                print(f'period: {period}, delta: {delta}')
                 # TODO be able to do something like struc_df[t1][t2]count+=1
                 # there has to be some algebraic relationship between t1, t2,
                 # and the pandas df index. I just need to figure it out, maybe 
                 # I'll ask abhijin to help me out
+                ind = (t2 - t1) + (period + delta)*(t1 - 1) - ((t1-2)*(t1-1))//2
+                print(f'ind: {ind}')
+                struc_df.iloc[ind]['count'] += 1
             j += 1
         edges[i][2]['delta'][str(delta)] = count
         bidirec_counts.append(count)
@@ -110,7 +123,6 @@ def main():
     # parge command line arguments
     parser = argparse.ArgumentParser(description='Get bidirectional motif counts in a temporal network.')
     parser.add_argument('source_file', help='Your source graph to count the bidirectional motifs on.')
-    # TODO add args for source column name, destination column name, timestamp column name
     parser.add_argument('source_column_index', type=int, help='column in file that contains source node locations')
     parser.add_argument('destination_column_index', type=int, help='column in file that contains destination node locations')
     parser.add_argument('timestamp_column_index', type=int, help='column in file that contains timestamp locations for edges')
@@ -122,11 +134,12 @@ def main():
     # read in the file and duplicate its edges for the appropiate delta, period
     df = read_file(args.source_file, args.separator, args.source_column_index, args.destination_column_index, args.timestamp_column_index)
     df_dup = dup_edges(df, delta=args.delta, period=args.period)
-    print(df_dup)
+    # print(df_dup)
 
     # set up the data structure to store stuff
-    data_struc = setup_data_struc(graph=df, graph_dup=df_dup, delta=args.delta)
-    print(data_struc)
+    data_struc = setup_data_struc(graph=df, graph_dup=df_dup, delta=args.delta, period=args.period)
+    # data_struc = setup_data_struc(graph=df, graph_dup=df_dup, delta=3, period=5)
+    # print(data_struc.to_string())
 
     # convert with nx stuff
     G = convert_nx(df_dup)
@@ -135,10 +148,11 @@ def main():
     list(G.edges(data=True))
     # count the bidirectional edges
     G.edges(data=True)
-    edges = bidirecCountAndStore(G, delta = 4, period = 12, struc_df=data_struc)
-    bidirecCountAndStore(G, delta = 2, period = 12, struc_df=data_struc)
+    edges = bidirecCountAndStore(G, delta = args.delta, period = args.period, struc_df=data_struc)
+    
     G = nx.relabel_nodes(G, nx.get_node_attributes(G, 'old_id'), copy=True)
-    print(G.edges(data=True))
+    # print(G.edges(data=True))
+    print(data_struc.to_string())
     
 
 if __name__ == '__main__':
